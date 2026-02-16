9661ea3e3f7d5d3d45b75affdac557dc6c16018f
qiulorie <lorieqiu04@gmail.com>
2026-02-16 14:24:11 +0100

    Entity

--- src/model/Entity.java
+++ src/model/Entity.java
@@ -1,4 +1,25 @@
 package src.model;
 
 public class Entity {
+    private String name;
+    private Stats stats;
+    private Inventory inventory;
+
+    public Entity(String name, Stats stats, Inventory inventory) {
+        this.name = name;
+        this.stats = stats;
+        this.inventory = inventory;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Stats getStats() {
+        return stats;
+    }
+
+    public Inventory getInventory() {
+        return inventory;
+    }
 }
